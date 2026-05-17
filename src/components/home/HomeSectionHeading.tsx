b45f64144be3101cd1dccb0f8fb8fab687ad02c3
Harris Azmi <azmi.harris@gmail.com>
2026-05-17 15:59:42 +0800

    fix: missed out color for sufficient contrast ratio for acessiblity

--- src/components/home/HomeSectionHeading.tsx
+++ src/components/home/HomeSectionHeading.tsx
@@ -8,7 +8,7 @@ type HomeSectionHeadingProps = {
 export function HomeSectionHeading({ label, action }: HomeSectionHeadingProps) {
   return (
     <div className="flex w-full items-center justify-between">
-      <h2 className="text-xs font-medium tracking-[2px] text-gray-130">{label}</h2>
+      <h2 className="text-xs font-medium tracking-[2px] text-gray-140">{label}</h2>
       {action}
     </div>
   )
